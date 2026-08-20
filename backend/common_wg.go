0bbb08ef75cbde256d59246f91bd2933aa0cb2bd
LiTar <fifalol92ya@gmail.com>
2026-08-20 20:05:30 +0300

    fix(network): возврат к старому списку статических IP для VK

--- backend/common_wg.go
+++ backend/common_wg.go
@@ -13,43 +13,21 @@ import (
 
 const wgIface = "wg-turn"
 
-var (
-	vkExcludeCIDRs []string
-	vkExcludeMu    sync.Mutex
-	vkExcludeOnce  sync.Once
-)
-
-// GetVKExcludeCIDRs возвращает IP-адреса серверов VK, критичных для авторизации и капчи.
+// GetVKExcludeCIDRs возвращает подсети VK, которые должны идти напрямую, а не через туннель.
 func GetVKExcludeCIDRs() []string {
-	vkExcludeOnce.Do(func() {
-		domains := []string{
-			"id.vk.ru",
-			"api.vk.ru",
-			"login.vk.com",
-			"oauth.vk.com",
-			"captcha.vk.com",
-		}
-		var ips []string
-		for _, d := range domains {
-			addrs, err := net.LookupIP(d)
-			if err == nil {
-				for _, addr := range addrs {
-					if ip4 := addr.To4(); ip4 != nil {
-						ips = append(ips, ip4.String()+"/32")
-					}
-				}
-			}
-		}
-		vkExcludeMu.Lock()
-		vkExcludeCIDRs = ips
-		vkExcludeMu.Unlock()
-	})
-
-	vkExcludeMu.Lock()
-	defer vkExcludeMu.Unlock()
-	res := make([]string, len(vkExcludeCIDRs))
-	copy(res, vkExcludeCIDRs)
-	return res
+	return []string{
+		"87.240.128.0/18",  // VK
+		"87.240.192.0/19",  // VK
+		"90.156.0.0/16",    // VK TURN (90.156.234.x, 90.156.236.x и др.)
+		"93.186.224.0/21",  // VK
+		"95.142.192.0/21",  // VK
+		"95.163.0.0/16",    // VK TURN (95.163.34.x и др.)
+		"95.213.0.0/18",    // VK (id.vk.ru, login.vk.com)
+		"155.212.192.0/20", // OK/VK (calls.okcdn.ru)
+		"185.16.28.0/22",   // VK
+		"194.67.64.0/18",   // VK
+		"195.82.146.0/23",  // VK
+	}
 }
 
 
